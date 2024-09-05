a0b5cb9dbd2234fb7bc91f183872447d81309def
Reikan <jcremoux@gmail.com>
2024-09-05 22:01:20 +0200

    Version 1.0.1 - Clean code - Add new event to ensure pet is summoned (after a fall, after AFK) - Rework settings panel

--- DontForgetMe.lua
+++ DontForgetMe.lua
@@ -1,6 +1,7 @@
 local DFM_Title
 local DFM_Author
 local DFM_Version
+local DFM_IsFalling = false
 
 -- Init saved settings
 if DFM_Settings == nil then
@@ -9,10 +10,6 @@ end
 
 if DFM_Settings.DFM_FavoriteOnly == nil then
     DFM_Settings.DFM_FavoriteOnly = true
-end
-
-if DFM_Settings.DFM_SyncPets == nil then
-    DFM_Settings.DFM_SyncPets = true
 end
 
 --- METHOD : On Load - Display Addon name and version
@@ -27,13 +24,19 @@ function DFM_OnLoad()
 
    -- Create option panel
     local panel = CreateFrame("Frame", nil, InterfaceOptionsFramePanelContainer)
-    panel.name = "Don't Forget Me"
+    panel.name = "Don't Forget Me!"
     local category = Settings.RegisterCanvasLayoutCategory(panel, panel.name)
     Settings.RegisterAddOnCategory(category)
 
+    -- Add an icon next to the title
+    local icon = panel:CreateTexture(nil, "ARTWORK")
+    icon:SetTexture("2341435")  -- Change this to the texture path or ID you want
+    icon:SetSize(32, 32)  -- Set the size of the icon
+    icon:SetPoint("TOPLEFT", 16, -16)  -- Adjust the position of the icon
+
     local title = panel:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
-    title:SetPoint("TOPLEFT", 16, -16)
-    title:SetText(DFM_Title .. " - " .. DFM_Version)
+    title:SetPoint("LEFT", icon, "RIGHT", 8, 0)  -- Position the title to the right of the icon
+    title:SetText("|cff0066d1"..DFM_Title.."|r".." - ".. DFM_Version)
 
     local author = panel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     author:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
@@ -41,21 +44,39 @@ function DFM_OnLoad()
     
     -- Favorite only check option
     local checkboxFavoriteOnly = CreateFrame("CheckButton", nil, panel, "UICheckButtonTemplate")
-    checkboxFavoriteOnly:SetPoint("TOPLEFT", author, "BOTTOMLEFT", 0, -16)
+    checkboxFavoriteOnly:SetPoint("TOPLEFT", author, "BOTTOMLEFT", 0, -10)
     checkboxFavoriteOnly.text:SetText("Summon favorite pets only")
     checkboxFavoriteOnly:SetChecked(DFM_Settings.DFM_FavoriteOnly)
-
     checkboxFavoriteOnly:SetScript("OnClick", function(self)
         DFM_Settings.DFM_FavoriteOnly = self:GetChecked()
     end)
 end
 
 -- METHOD : Check if a pet is summon, if not, summon a random favorite pet
-local function DFM_EnsurePet()
+local function DFM_SummonPet()
     local DFM_petGUID = C_PetJournal.GetSummonedPetGUID()
 
-    if not DFM_petGUID then
+    -- If player has not current pet summoned
+    if (not DFM_petGUID) then
         C_PetJournal.SummonRandomPet(DFM_Settings.DFM_FavoriteOnly)
+    end
+end
+
+-- METHOD : On player flag changed (after an AFK)
+local function OnPlayerFlagsChanged()
+    local isAFK = UnitIsAFK("player")
+    if (not isAFK) then
+        DFM_SummonPet()
+    end
+end
+
+-- METHOD : Update falling state
+local function OnUpdate()
+    if (IsFalling()) then
+        DFM_IsFalling = true
+    elseif (DFM_IsFalling) then
+        DFM_SummonPet()
+        DFM_IsFalling = false
     end
 end
 
@@ -68,22 +89,28 @@ f:RegisterEvent("PLAYER_REGEN_ENABLED")
 f:RegisterEvent("PLAYER_LOGIN")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
-f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("PLAYER_UNGHOST")
+f:RegisterEvent("PLAYER_FLAGS_CHANGED")
 
 -- Manage events
-f:SetScript("OnEvent", function(self, event, ...)
+f:SetScript("OnEvent", function(self, event, unit, ...)
     local arg1, arg2, arg3, arg4 = ...
 
-    if event == "PLAYER_LOGIN" then
+    if (event == "PLAYER_LOGIN") then
         DFM_OnLoad()
-    elseif event == "ZONE_CHANGED_NEW_AREA" or event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_REGEN_ENABLED" then
-        DFM_EnsurePet()
-    elseif event == "PLAYER_MOUNT_DISPLAY_CHANGED" then
+    elseif (event == "ZONE_CHANGED_NEW_AREA" or event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_REGEN_ENABLED" or event == "PLAYER_UNGHOST") then
+        DFM_SummonPet()
+    elseif (event == "PLAYER_MOUNT_DISPLAY_CHANGED") then
         local isMounted = IsMounted()
         if not isMounted then
-            DFM_EnsurePet()
-        end        
-    elseif event == "CHAT_MSG_ADDON" then
-        DFM_OnAddonMessage(arg1, arg2, arg3, arg4)
+            DFM_SummonPet()
+        end   
+    elseif (event == "PLAYER_FLAGS_CHANGED") then
+        if unit == "player" then
+            OnPlayerFlagsChanged()
+        end
     end
 end)
+
+-- Update check for falling state
+f:SetScript("OnUpdate", OnUpdate)
