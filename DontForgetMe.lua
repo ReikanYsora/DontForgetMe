ec888af600f4427a0a451dbc74399754714c15ce
Reikan <jcremoux@gmail.com>
2024-09-06 21:18:50 +0200

    Version 1.0.2 - Fix for stealth

--- DontForgetMe.lua
+++ DontForgetMe.lua
@@ -2,6 +2,7 @@ local DFM_Title
 local DFM_Author
 local DFM_Version
 local DFM_IsFalling = false
+local DFM_IsStealthed = false -- Nouvelle variable pour suivre l'état de furtivité
 
 -- Init saved settings
 if DFM_Settings == nil then
@@ -17,7 +18,7 @@ function DFM_OnLoad()
     DFM_Title = C_AddOns.GetAddOnMetadata("DontForgetMe", "Title")
     DFM_Author = C_AddOns.GetAddOnMetadata("DontForgetMe", "Author")
     DFM_Version = C_AddOns.GetAddOnMetadata("DontForgetMe", "Version")
-    DEFAULT_CHAT_FRAME:AddMessage("|cff0066d1".. DFM_Title .."|r |cffffff00-|r |cffeb7800"..DFM_Version.."|r |cffffff00 by "..DFM_Author.."|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cff0066d1".. DFM_Title .."|r - |cffeb7800"..DFM_Version.."|r by "..DFM_Author)
 
     -- Register addon message prefix
     C_ChatInfo.RegisterAddonMessagePrefix("DFM_PetSync")
@@ -30,17 +31,17 @@ function DFM_OnLoad()
 
     -- Add an icon next to the title
     local icon = panel:CreateTexture(nil, "ARTWORK")
-    icon:SetTexture("2341435")  -- Change this to the texture path or ID you want
-    icon:SetSize(32, 32)  -- Set the size of the icon
-    icon:SetPoint("TOPLEFT", 16, -16)  -- Adjust the position of the icon
+    icon:SetTexture("2341435") -- Change this to the texture path or ID you want
+    icon:SetSize(32, 32) -- Set the size of the icon
+    icon:SetPoint("TOPLEFT", 16, -16) -- Adjust the position of the icon
 
     local title = panel:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
-    title:SetPoint("LEFT", icon, "RIGHT", 8, 0)  -- Position the title to the right of the icon
+    title:SetPoint("LEFT", icon, "RIGHT", 8, 0) -- Position the title to the right of the icon
     title:SetText("|cff0066d1"..DFM_Title.."|r".." - ".. DFM_Version)
 
     local author = panel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     author:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-    author:SetText("by " .. DFM_Author)
+    author:SetText("by "..DFM_Author)
     
     -- Favorite only check option
     local checkboxFavoriteOnly = CreateFrame("CheckButton", nil, panel, "UICheckButtonTemplate")
@@ -52,20 +53,28 @@ function DFM_OnLoad()
     end)
 end
 
--- METHOD : Check if a pet is summon, if not, summon a random favorite pet
+-- METHOD : Check if a pet is summoned, if not, summon a random favorite pet
 local function DFM_SummonPet()
     local DFM_petGUID = C_PetJournal.GetSummonedPetGUID()
 
-    -- If player has not current pet summoned
-    if (not DFM_petGUID) then
-        C_PetJournal.SummonRandomPet(DFM_Settings.DFM_FavoriteOnly)
+    if (DFM_IsStealthed == false) then
+        -- If player has not current pet summoned, summon it
+        if (not DFM_petGUID) then
+            C_PetJournal.SummonRandomPet(DFM_Settings.DFM_FavoriteOnly)
+        end
+    else
+        -- If player has pet summoned, dismiss it
+        if (DFM_petGUID) then
+            C_PetJournal.SummonPetByGUID(C_PetJournal.GetSummonedPetGUID())
+        end
     end
 end
 
 -- METHOD : On player flag changed (after an AFK)
 local function OnPlayerFlagsChanged()
     local isAFK = UnitIsAFK("player")
-    if (not isAFK) then
+
+    if (isAFK == false) and (DFM_IsStealthed == false) then
         DFM_SummonPet()
     end
 end
@@ -80,6 +89,17 @@ local function OnUpdate()
     end
 end
 
+-- METHOD : On stealth state changed
+local function OnStealthStateChanged()
+    if IsStealthed() then
+        DFM_IsStealthed = true
+    else
+        DFM_IsStealthed = false
+    end
+    
+    DFM_SummonPet()
+end
+
 -- Create frame for events
 local f = CreateFrame("Frame")
 
@@ -91,6 +111,7 @@ f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
 f:RegisterEvent("PLAYER_UNGHOST")
 f:RegisterEvent("PLAYER_FLAGS_CHANGED")
+f:RegisterEvent("UPDATE_STEALTH")
 
 -- Manage events
 f:SetScript("OnEvent", function(self, event, unit, ...)
@@ -109,6 +130,8 @@ f:SetScript("OnEvent", function(self, event, unit, ...)
         if unit == "player" then
             OnPlayerFlagsChanged()
         end
+    elseif (event == "UPDATE_STEALTH") then
+        OnStealthStateChanged()
     end
 end)
 
